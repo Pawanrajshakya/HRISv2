cb199fe2f7fb4da28a48ac00488d993e482720b3
Shakya, Pawan Raj <shakyapj@dss.nyc.gov>
2022-07-15 10:00:50 -0400

    Staff Detail

--- HRIS.API/Models/Charts/Dto/TEAM_CaseCountByYearChartDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRIS.API
-{
-    public class Team_CaseCountByYearChartDto
-    {
-        public string Flag { get; set; }
-        public int Count { get; set; }
-        public string Year { get; set; }
-    }
-}
