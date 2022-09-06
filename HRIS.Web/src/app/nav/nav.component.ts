be0ee1a4ddfaa7b1fb29add551ecb3a3e466ecc4
Shakya, Pawan Raj <shakyapj@dss.nyc.gov>
2022-09-06 14:29:37 -0400

    Classification Report

--- HRIS.Web/src/app/nav/nav.component.ts
+++ HRIS.Web/src/app/nav/nav.component.ts
@@ -1,19 +1,23 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ICurrentUser } from "../_models/ICurrentUser";
+import { ICurrentUser } from '../_models/ICurrentUser';
 import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 import { LoginService } from '../_services/login.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
-  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  providers: [
+    {
+      provide: BsDropdownConfig,
+      useValue: { isAnimated: true, autoClose: true },
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class NavComponent {
-
   currentUser$: Observable<ICurrentUser>;
 
   message: any;
@@ -21,4 +25,18 @@ export class NavComponent {
     this.currentUser$ = this.loginService.currentUserAction$;
   }
 
+  onClassificationReportClick(i: number) {
+    console.log(i);
+    if (i === 1) {
+      window.open(
+        'https://rs16reports.hra.nycnet/ReportServer/Pages/ReportViewer.aspx?%2FOSR%20Reports%2FCOD%20Reporting%20Log&rs%3ACommand=Render&viewAll=False&RCs=',
+        '_blank'
+      );
+    } else {
+      window.open(
+        'https://rs16reports.hra.nycnet/ReportServer/Pages/ReportViewer.aspx?%2FOSR%20Reports%2FCOD%20Reporting%20Log&rs%3ACommand=Render&viewAll=True&RCs=',
+        '_blank'
+      );
+    }
+  }
 }
